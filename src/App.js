00ef5a387d506a035e332ce61e82a7364d9630c8
Marek Gacek <gacekmarek92@gmail.com>
2018-08-23 21:46:20 +0200

    Code refactor

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
-import { GoogleApiWrapper } from 'google-maps-react'
-import './App.css';
-import MapContainer from './MapContainer'
- class App extends Component {
+import React, { Component } from "react";
+import { GoogleApiWrapper } from "google-maps-react";
+import "./App.css";
+import MapContainer from "./MapContainer";
+class App extends Component {
   render() {
     return (
       <div>
         <a className="menu" tabIndex="0">
-          <svg className="hamburger-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path d="M2 6h20v3H2zm0 5h20v3H2zm0 5h20v3H2z"/>
+          <svg
+            className="hamburger-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <path d="M2 6h20v3H2zm0 5h20v3H2zm0 5h20v3H2z" />
           </svg>
         </a>
         <h1 className="heading"> Udacity Google Map + React Project </h1>
@@ -17,6 +21,6 @@ import MapContainer from './MapContainer'
     );
   }
 }
- export default GoogleApiWrapper({
-  apiKey: 'AIzaSyBqeCAur3WuwLz9vaZyfuVA4WzfqSFjmiM'
-})(App)
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyBqeCAur3WuwLz9vaZyfuVA4WzfqSFjmiM"
+})(App);
--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,168 +1,196 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom'
- export default class MapContainer extends Component {
-
-   state = {
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+export default class MapContainer extends Component {
+  state = {
     locations: [
-      { name: "Museum of Contemporary Art", location: {lat: 51.108015, lng: 17.075859} },
-      { name: "Hydropolis", location: {lat: 51.104254, lng: 17.056610} },
-      { name: "Wroclaw Town Hall", location: {lat: 51.109696, lng: 17.032106} },
-      { name: "Centennial Hall", location: {lat: 51.106766, lng: 17.077152} },
-      { name: "Cathedral Island", location: {lat: 51.114616, lng: 17.046704} },
-      { name: "Panorama of the Battle of Racławice", location: {lat: 51.110142, lng: 17.044386} },
-      { name: "University Museum - Aula Leopoldina", location: {lat: 51.114020, lng:  17.034510} },
-      { name: "Tumski Bridge", location: {lat: 51.114709, lng: 17.042330} },
-      { name: "ZOO", location: {lat: 51.104469, lng:  17.075200}},
-      { name: "Main railway station", location: {lat: 51.098784, lng:  17.036510}}
-
+      {
+        name: "Museum of Contemporary Art",
+        location: { lat: 51.108015, lng: 17.075859 }
+      },
+      { name: "Hydropolis", location: { lat: 51.104254, lng: 17.05661 } },
+      {
+        name: "Wroclaw Town Hall",
+        location: { lat: 51.109696, lng: 17.032106 }
+      },
+      { name: "Centennial Hall", location: { lat: 51.106766, lng: 17.077152 } },
+      {
+        name: "Cathedral Island",
+        location: { lat: 51.114616, lng: 17.046704 }
+      },
+      {
+        name: "Panorama of the Battle of Racławice",
+        location: { lat: 51.110142, lng: 17.044386 }
+      },
+      {
+        name: "University Museum - Aula Leopoldina",
+        location: { lat: 51.11402, lng: 17.03451 }
+      },
+      { name: "Tumski Bridge", location: { lat: 51.114709, lng: 17.04233 } },
+      { name: "ZOO", location: { lat: 51.104469, lng: 17.0752 } },
+      {
+        name: "Main railway station",
+        location: { lat: 51.098784, lng: 17.03651 }
+      }
     ],
-    query: '',
+    query: "",
     markers: [],
     infowindow: new this.props.google.maps.InfoWindow(),
-     changeIcon: null
-  }
+    changeIcon: null
+  };
 
-   componentDidMount() {
-    this.loadMap()
-     this.onclickPlace()
-     // Create a "highlighted location" marker color for when the user
+  componentDidMount() {
+    this.loadMap();
+    this.onclickPlace();
+    // Create a "highlighted location" marker color for when the user
     // clicks on the marker.
-    this.setState({changeIcon: this.makeMarkerIcon('2486ff')})
-   }
-   loadMap() {
+    this.setState({ changeIcon: this.makeMarkerIcon("2486ff") });
+  }
+  loadMap() {
     if (this.props && this.props.google) {
-      const {google} = this.props
-      const maps = google.maps
-       const mapRef = this.refs.map
-      const node = ReactDOM.findDOMNode(mapRef)
-       const mapConfig = Object.assign({}, {
-        center: {lat: 51.107883, lng: 17.038538},
-        zoom: 13,
-        mapTypeId: 'roadmap'
-      })
-       this.map = new maps.Map(node, mapConfig)
-       this.addMarkers()
+      const { google } = this.props;
+      const maps = google.maps;
+      const mapRef = this.refs.map;
+      const node = ReactDOM.findDOMNode(mapRef);
+      const mapConfig = Object.assign(
+        {},
+        {
+          center: { lat: 51.107883, lng: 17.038538 },
+          zoom: 13,
+          mapTypeId: "roadmap"
+        }
+      );
+      this.map = new maps.Map(node, mapConfig);
+      this.addMarkers();
     }
-   }
-
-   onclickPlace = () => {
-
-    const {infowindow} = this.state
-     const displayInfowindow = (e) => {
-      const {markers} = this.state
-      const markerIndex = markers.findIndex(m => m.title.toLowerCase() === e.target.innerText.toLowerCase())
-      this.populateInfoWindow(markers[markerIndex], infowindow)
-    }
-    document.querySelector('.locations-list').addEventListener('click', function (e) {
-      if(e.target && e.target.nodeName === "LI") {
-        displayInfowindow(e)
-      }
-    })
   }
 
-  whenValueChange = (e) => {
-      this.setState({query: e.target.value})
-    }
+  onclickPlace = () => {
+    const { infowindow } = this.state;
+    const displayInfowindow = e => {
+      const { markers } = this.state;
+      const markerIndex = markers.findIndex(
+        m => m.title.toLowerCase() === e.target.innerText.toLowerCase()
+      );
+      this.populateInfoWindow(markers[markerIndex], infowindow);
+    };
+    document
+      .querySelector(".locations-list")
+      .addEventListener("click", function(e) {
+        if (e.target && e.target.nodeName === "LI") {
+          displayInfowindow(e);
+        }
+      });
+  };
 
-   addMarkers = () => {
-    const {google} = this.props
-    let {infowindow} = this.state
+  whenValueChange = e => {
+    this.setState({ query: e.target.value });
+  };
+
+  addMarkers = () => {
+    const { google } = this.props;
+    let { infowindow } = this.state;
     const bounds = new google.maps.LatLngBounds();
-     this.state.locations.forEach( (location, ind) => {
+    this.state.locations.forEach((location, ind) => {
       const marker = new google.maps.Marker({
-        position: {lat: location.location.lat, lng: location.location.lng},
+        position: { lat: location.location.lat, lng: location.location.lng },
         map: this.map,
         animation: google.maps.Animation.DROP,
         title: location.name
       });
-       marker.addListener('click', () => {
-        this.populateInfoWindow(marker, infowindow)
-      })
-      this.setState((state) => ({
+      marker.addListener("click", () => {
+        this.populateInfoWindow(marker, infowindow);
+      });
+      this.setState(state => ({
         markers: [...state.markers, marker]
-      }))
-      bounds.extend(marker.position)
-    })
+      }));
+      bounds.extend(marker.position);
+    });
     // Extend the boundaries of the map for each marker
-    this.map.fitBounds(bounds)
-  }
+    this.map.fitBounds(bounds);
+  };
   // This function populates the infowindow when the marker is clicked. We'll only allow
-       // one infowindow which will open at the marker that is clicked, and populate based
-       // on that markers position.
-       populateInfoWindow = (marker, infowindow) => {
-         const defaultIcon = marker.getIcon()
-   const {changeIcon, markers} = this.state
-         // Check to make sure the infowindow is not already opened on this marker.
-         if (infowindow.marker !== marker) {
-           // reset the color of previous marker
-     if (infowindow.marker) {
-       const ind = markers.findIndex(m => m.title === infowindow.marker.title)
-       markers[ind].setIcon(defaultIcon)
-     }
-     // change marker icon color of clicked marker
-     marker.setIcon(changeIcon)
-           infowindow.marker = marker;
-           infowindow.setContent('<div>' + marker.title + '</div>');
-           infowindow.open(this.map, marker);
-           // Make sure the marker property is cleared if the infowindow is closed.
-           infowindow.addListener('closeclick',function(){
-             infowindow.setMarker = null;
-           });
-         }
-       }
-       // This function takes in a COLOR, and then creates a new marker
-            // icon of that color. The icon will be 20 px wide by 30 high, have an origin
-            // of 0, 0 and be anchored at 10, 34).
-          makeMarkerIcon = (markerColor) => {
-            const {google} = this.props
-              var markerImage = new google.maps.MarkerImage(
-                'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-                '|40|_|%E2%80%A2',
-                new google.maps.Size(20, 30),
-                new google.maps.Point(0, 0),
-                new google.maps.Point(10, 34),
-                new google.maps.Size(21,34));
-              return markerImage;
-            }
-   render() {
-     const { locations, query, markers, infowindow} = this.state
+  // one infowindow which will open at the marker that is clicked, and populate based
+  // on that markers position.
+  populateInfoWindow = (marker, infowindow) => {
+    const defaultIcon = marker.getIcon();
+    const { changeIcon, markers } = this.state;
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker !== marker) {
+      // reset the color of previous marker
+      if (infowindow.marker) {
+        const ind = markers.findIndex(m => m.title === infowindow.marker.title);
+        markers[ind].setIcon(defaultIcon);
+      }
+      // change marker icon color of clicked marker
+      marker.setIcon(changeIcon);
+      infowindow.marker = marker;
+      infowindow.setContent("<div>" + marker.title + "</div>");
+      infowindow.open(this.map, marker);
+      // Make sure the marker property is cleared if the infowindow is closed.
+      infowindow.addListener("closeclick", function() {
+        infowindow.setMarker = null;
+      });
+    }
+  };
+  // This function takes in a COLOR, and then creates a new marker
+  // icon of that color. The icon will be 20 px wide by 30 high, have an origin
+  // of 0, 0 and be anchored at 10, 34).
+  makeMarkerIcon = markerColor => {
+    const { google } = this.props;
+    var markerImage = new google.maps.MarkerImage(
+      "http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|" +
+        markerColor +
+        "|40|_|%E2%80%A2",
+      new google.maps.Size(20, 30),
+      new google.maps.Point(0, 0),
+      new google.maps.Point(10, 34),
+      new google.maps.Size(21, 34)
+    );
+    return markerImage;
+  };
+  render() {
+    const { locations, query, markers, infowindow } = this.state;
     if (query) {
-      locations.forEach((l,i) => {
-        if(l.name.toLowerCase().includes(query.toLowerCase())) {
-          markers[i].setVisible(true)
+      locations.forEach((l, i) => {
+        if (l.name.toLowerCase().includes(query.toLowerCase())) {
+          markers[i].setVisible(true);
         } else {
-          if (infowindow.marker === markers[i]){
+          if (infowindow.marker === markers[i]) {
             // close the info window if marker removed
-            infowindow.close()
+            infowindow.close();
           }
-          markers[i].setVisible(false)
+          markers[i].setVisible(false);
         }
-      })
+      });
     } else {
-      locations.forEach((l,i) => {
+      locations.forEach((l, i) => {
         if (markers.length && markers[i]) {
-          markers[i].setVisible(true)
+          markers[i].setVisible(true);
         }
-      })
+      });
     }
     return (
       <div>
         <div className="container">
-        <div className="text-input">
-        <input role="search" type='text'
-        placeholder="Enter your favourite place!"
-                   value={this.state.value}
-                   onChange={this.whenValueChange}/>
-          <ul className="locations-list">{
-          markers.filter(m => m.getVisible()).map((m, i) =>
-              (<li key={i}>{m.title}</li>))
-          }</ul>
+          <div className="text-input">
+            <input
+              role="search"
+              type="text"
+              placeholder="Enter your favourite place!"
+              value={this.state.value}
+              onChange={this.whenValueChange}
+            />
+            <ul className="locations-list">
+              {markers.filter(m => m.getVisible()).map((m, i) => (
+                <li key={i}>{m.title}</li>
+              ))}
+            </ul>
           </div>
           <div role="application" className="map" ref="map">
             loading map...
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
